3f573030152104235c6a576370f8ca1ad9ca63a9
Frankie <gruzmanboyq@gmail.com>
2023-04-25 23:39:46 -0600

    Complete qualified assignment, ...

---
+++ public/src/accounts.js
@@ -0,0 +1,45 @@
+function findAccountById(accounts, id) {
+  return accounts.some((account) => 
+  account.id.includes(id)) ? accounts.find((account) => 
+  account.id.includes(id)) : null;
+}
+
+function sortAccountsByLastName(accounts) {
+ const sortLastName = accounts.sort((accountA,accountB) => 
+ (accountA.name.last < accountB.name.last ? -1 : 1))
+ return sortLastName;
+}
+
+function getTotalNumberOfBorrows(account, books) {
+  return books.reduce((acc, book) => {
+    const borrows = book.borrows;
+    const numOfBorrows = borrows.reduce((borrowAcc, borrow) => {
+      return borrow.id === account.id ? borrowAcc + 1 : borrowAcc;
+    }, 0);
+    return acc + numOfBorrows;
+  }, 0);
+}
+
+
+function getBooksPossessedByAccount(account, books, authors) {
+  const accountId = account.id;
+  const checkedOutBooks = books.filter((book) => {
+    const borrow = book.borrows[0];
+    return borrow.id === accountId && !borrow.returned;
+  });
+  const booksWithAuthorInfo = checkedOutBooks.map((book) => {
+    const author = authors.find((author) => author.id === book.authorId)
+    return {
+      ...book,
+      author: author
+    };
+  });
+  return booksWithAuthorInfo
+}
+
+module.exports = {
+  findAccountById,
+  sortAccountsByLastName,
+  getTotalNumberOfBorrows,
+  getBooksPossessedByAccount,
+};
---
+++ public/src/home.js
@@ -0,0 +1,80 @@
+function getTotalBooksCount(books) {
+  var total = 0;
+  const totalBooks = books.map((book) => book.id);
+  const bookCount = totalBooks.forEach(() => {
+     total ++
+  })
+    
+  
+  return total;
+}
+
+function getTotalAccountsCount(accounts) {
+  let total = 0;
+  for (let i = 0; i < accounts.length; i++) {
+    total++;
+  }
+  return total;
+}
+
+function getBooksBorrowedCount(books) {
+  let count = 0;
+  for (let i = 0; i < books.length; i++) {
+    const borrow = books[i].borrows[0];
+    if (!borrow.returned) {
+      count++;
+    }
+  }
+  return count;
+}
+
+function getMostCommonGenres(books) {
+  let obj = {};
+  books.forEach((book) => {
+    if(obj[book.genre]){
+      obj[book.genre]++;
+    } else {
+      obj[book.genre] = 1;
+    }
+  });
+  let genreCount = [];
+  //const entries = Object.entries(obj);
+  //for (const [key, value] of entries)
+  const keys = Object.keys(obj);
+
+    /*entries.forEach(([key,value]) => 
+    genreCount.push({
+      'name' : key,
+      'count' : value
+    }) */
+    keys.forEach((key) => 
+    genreCount.push({
+      'name' : key,
+      'count' : obj[key]
+    })
+  )
+  genreCount.sort((a, b) => b.count - a.count);
+  return genreCount.slice(0, 5);
+}
+
+function getMostPopularBooks(books) {
+  return books.map((book) => 
+  {return {name: book.title, count: book.borrows.length}}).sort((a, b) => 
+  (a.count < b.count ? 1 : -1)).slice(0,5)
+}
+function getMostPopularAuthors(books, authors) {
+  return books.map((book) => {
+    return { name: authors.find((author) => 
+      author.id == book.authorId).name.first + ' ' + authors.find((author) => 
+      author.id == book.authorId).name.last, count: book.borrows.length}}).sort((a, b) => 
+      (a.count < b.count ? 1 : -1)).slice(0,5)
+}
+
+module.exports = {
+  getTotalBooksCount,
+  getTotalAccountsCount,
+  getBooksBorrowedCount,
+  getMostCommonGenres,
+  getMostPopularBooks,
+  getMostPopularAuthors,
+};
